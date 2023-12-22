7dfee913ddfed58e69f7901c470156f9ab061488
ALi Omar <aliomaralsloom@gmail.com>
2023-12-22 20:24:33 +0300

    first commit

---
+++ carRental/src/pages/imports.js
@@ -0,0 +1,7 @@
+// export { default as } from "./about/About"
+export {default as BookCar} from "./home/book/BookCar.jsx";
+export {default as DownloadApp} from "./home/downloadApp/DownloadApp";
+export {default as Faq} from "./home/faq/Faq";
+export {default as Hero} from "./home/hero/Hero";
+export {default as VehicleModels} from "./home/vehicleModels/VehicleModels";
+export {default as WhyChooseUs} from "./home/whyChooseUs/WhyChooseUs";
