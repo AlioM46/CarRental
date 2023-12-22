920cc0084519700e53ff4a37296e3456d9c2243b
ALi Omar <aliomaralsloom@gmail.com>
2023-12-22 20:51:19 +0300

    edit import.js file #2

--- carRental/src/pages/imports.js
+++ carRental/src/pages/imports.js
@@ -1,7 +1,7 @@
 // export { default as } from "./about/About"
 export {default as BookCar} from "./home/book/BookCar.jsx";
 export {default as DownloadApp} from "./home/downloadApp/downloadApp.jsx";
-export {default as Faq} from "./home/faq/Faq";
-export {default as Hero} from "./home/hero/Hero";
-export {default as VehicleModels} from "./home/vehicleModels/VehicleModels";
-export {default as WhyChooseUs} from "./home/whyChooseUs/WhyChooseUs";
+export {default as Faq} from "./home/faq/Faq.jsx";
+export {default as Hero} from "./home/hero/Hero.jsx";
+export {default as VehicleModels} from "./home/vehicleModels/vehicleModels.jsx";
+export {default as WhyChooseUs} from "./home/whyChooseUs/WhyChooseUs.jsx";
